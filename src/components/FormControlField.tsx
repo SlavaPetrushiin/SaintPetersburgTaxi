93533b9226bd0540cba04d526b35b57f4fb24ec7
Slava <slava91petrushin@yandex.ru>
2020-08-05 22:56:55 +0300

    added FormControlField

---
+++ src/components/FormControlField.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+type IProps = {
+	type: string
+	title: string
+	name: string
+	id: string
+	value: string
+	required: boolean
+	onChange: (e: React.ChangeEvent<HTMLInputElement>, name: string) => void
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		form: {
+			marginBottom: 20,
+			width: "100%"
+		},
+		input: {
+			width: "100%"
+		}
+	}),
+);
+
+const FormControlField = (props: IProps) => {
+	const classes = useStyles();
+
+	return (
+		<FormControl className={classes.form}>
+			<InputLabel htmlFor={props.id}>{props.title}</InputLabel>
+			<Input
+				type={props.type}
+				id={props.id}
+				name={props.name}
+				required={props.required}
+				value={props.value}
+				onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.onChange(e, props.name)}
+				className={classes.input}
+				startAdornment={
+					<InputAdornment position="start">
+						<AccountCircle />
+					</InputAdornment>
+				}
+			/>
+		</FormControl>
+	);
+}
+
+export default FormControlField;
--- src/page/authenticationPage/AuthenticationPage.tsx
+++ src/page/authenticationPage/AuthenticationPage.tsx
@@ -3,18 +3,41 @@ import BackgroundPage from '../../components/BackgroundPage';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import AccountCircle from '@material-ui/icons/AccountCircle';
 import { Button } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import { useDispatch } from 'react-redux';
 import {fetchLogin} from '../../store/signIn/signInReducer'
 import { RootState } from '../../store/store';
 import { useSelector } from 'react-redux';
+import FormControlField from '../../components/FormControlField';
 
+type IInput = {
+	type: string
+	title: string
+	name: string
+	id: string
+	value: string
+	required: boolean
+}
+
+const inputs: Array<IInput>  = [
+	{
+		type: "email",
+		name: "email",
+		title: "Email",
+		id: "email",
+		value: "",
+		required: true
+	},
+	{
+		type: "password",
+		name: "password",
+		title: "Password",
+		id: "password",
+		value: "",
+		required: true
+	},	
+]
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
@@ -35,19 +58,25 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const AuthenticationPage = () => {
 	const classes = useStyles();
+	const [fields, setFields] = useState<IInput[]>(inputs)
 	const dispatch = useDispatch();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
 	const [disabled, setDisabled] = useState(false);
 	const error = useSelector((store: RootState) => store.signIn.error ); 
 
 	const login = async () => {
-		await setDisabled(true);
-		await dispatch(fetchLogin(email, password));
-		await setDisabled(false);
-		await setEmail("");
-		await setPassword("");
+		setDisabled(true);
+		//await dispatch(fetchLogin(email, password));
+		setDisabled(false);
+	};
 
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>, name: string) => {
+		let newFields = fields.map((field: IInput) => {
+			if(field.name === name){
+				field.value = e.currentTarget.value;
+			}
+			return field;
+		})
+		setFields(newFields)
 	};
 
 	return (
@@ -57,35 +86,9 @@ const AuthenticationPage = () => {
 					<Typography variant="h6" gutterBottom>
         		Войти
       		</Typography>
-					<FormControl className={classes.form}>
-						<InputLabel htmlFor="email">Email</InputLabel>
-						<Input
-							id="email"
-							required={true}
-							value={email}
-							onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.currentTarget.value)}
-							className={classes.input}
-							startAdornment={
-								<InputAdornment position="start">
-									<AccountCircle />
-								</InputAdornment>
-							}
-						/>
-					</FormControl>
-					<FormControl className={classes.form}>
-						<InputLabel htmlFor="password">Password</InputLabel>
-						<Input
-							id="password"
-							required={true}
-							value={password}
-							onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.currentTarget.value)}
-							startAdornment={
-								<InputAdornment position="start">
-									<AccountCircle />
-								</InputAdornment>
-							}
-						/>
-					</FormControl>
+					{
+						fields.map((inp: any) => <FormControlField {...inp} onChange={handleChange} />)
+					}
 					{
 						error !== null && <p className={classes.error}>{error}</p>
 					}
