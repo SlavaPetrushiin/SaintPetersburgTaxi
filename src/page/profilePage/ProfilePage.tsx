f55970786b29f0df35bf4bede7ebe5513c14bd76
Slava <slava91petrushin@yandex.ru>
2020-08-14 22:06:20 +0300

    mask

--- src/page/profilePage/ProfilePage.tsx
+++ src/page/profilePage/ProfilePage.tsx
@@ -1,9 +1,37 @@
 import React, { useState } from 'react';
 import BackgroundPage from '../../components/BackgroundPage';
 import { useSelector } from 'react-redux';
+import { Card } from '@material-ui/core';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+//@ts-ignore
+import MaskedInput from 'react-text-mask';
 //@ts-ignore
 import Cards from 'react-credit-cards';
-import { Card } from '@material-ui/core';
+
+interface TextMaskCustomProps {
+	inputRef: (ref: HTMLInputElement | null) => void;
+}
+
+function TextMaskCustom(props: TextMaskCustomProps) {
+	const { inputRef, ...other } = props;
+
+	return (
+		<MaskedInput
+			{...other}
+			ref={(ref: any) => {
+				inputRef(ref ? ref.inputElement : null);
+			}}
+			mask={[ /[1-9]/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+			placeholderChar={'\u2000'}
+			showMask={true}
+			guide={true}
+		/>
+	);
+}
+
 
 const ProfilePage = () => {
 	const [state, setState] = useState({
@@ -14,6 +42,17 @@ const ProfilePage = () => {
 		number: '',
 	});
 
+  const [values, setValues] = useState({
+    textmask: '0000-0000-0000-0000',
+    numberformat: '1320',
+	});
+	
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
 
 	return (
 		<BackgroundPage>
@@ -26,17 +65,21 @@ const ProfilePage = () => {
 						name={state.name}
 						number={state.number}
 					/>
-					<form>
-						<input
-							type="tel"
-							name="number"
-							placeholder="Card Number"
+					<FormControl>
+						<InputLabel htmlFor="formatted-text-mask-input">react-text-mask</InputLabel>
+						<Input
+							value={values.textmask}
+							onChange={handleChange}
+							name="textmask"
+							id="formatted-text-mask-input"
+							inputComponent={TextMaskCustom as any}
 						/>
-          ...
-        </form>
+					</FormControl>
+
+
 				</div>
 			</Card>
-		</BackgroundPage>
+		</BackgroundPage >
 	)
 };
 
